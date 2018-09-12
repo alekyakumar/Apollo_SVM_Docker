3ee934a7fd2c2ba581576b6206a47d41235b425c
Alekya Kumar <35089654+alekyakumar@users.noreply.github.com>
2018-09-12 15:54:39 -0400

    Update jupyter_notebook_config.py

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -17,6 +17,23 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
+# import os
+# from IPython.lib import passwd
+
+# c = c  # pylint:disable=undefined-variable
+# c.NotebookApp.ip = '*'
+# c.NotebookApp.port = int(os.getenv('PORT', 8888))
+# c.NotebookApp.open_browser = False
+
+# # sets a password if PASSWORD is set in the environment
+# if 'PASSWORD' in os.environ:
+#   password = os.environ['PASSWORD']
+#   if password:
+#     c.NotebookApp.password = passwd(password)
+#     else:
+#     c.NotebookApp.password = ''
+#     c.NotebookApp.token = ''
+#     del os.environ['PASSWORD']
 import os
 from IPython.lib import passwd
 
@@ -27,11 +44,12 @@ c.NotebookApp.open_browser = False
 
 # sets a password if PASSWORD is set in the environment
 if 'PASSWORD' in os.environ:
-  password = os.environ['PASSWORD']
-  if password:
-    c.NotebookApp.password = passwd(password)
+    password = os.environ['PASSWORD']
+    if password:
+        c.NotebookApp.password = passwd(password)
     else:
-    c.NotebookApp.password = ''
-    c.NotebookApp.token = ''
-    del os.environ['PASSWORD']
+        c.NotebookApp.password = ''
+        c.NotebookApp.token = ''
+        del os.environ['PASSWORD']
+
 
